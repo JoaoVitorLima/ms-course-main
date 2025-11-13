72a988b6742761654eb544e8a168eda6be18cd72
JoaoVitorLima <joaovitorlo@outlook.com>
2025-11-13 11:26:04 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/com/devsuperior/hroauth/entities/Role.java
@@ -0,0 +1,46 @@
+package com.devsuperior.hroauth.entities;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Role implements Serializable {
+
+    private Long id;
+    private String roleName;
+
+    public Role() {
+    }
+
+    public Role(Long id, String roleName) {
+        this.id = id;
+        this.roleName = roleName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Role)) return false;
+        Role role = (Role) o;
+        return Objects.equals(getId(), role.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getId());
+    }
+}
